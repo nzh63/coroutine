068001c76a2b66fbc23abf348a689782cd4b26d9
nzh63 <nzh21@outlook.com>
2022-05-03 23:03:39 +0800

    更新版权年份

--- src/arch/x64.S
+++ src/arch/x64.S
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 nzh63
+ * Copyright 2020-present nzh63
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
